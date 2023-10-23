5d5e8d9aefbd8c426a0214ae029fb63ad6569526
erikg <gutierrezerik971@gmail.com>
2023-10-23 18:44:04 -0500

    Revert "added cube demo"
    This reverts commit 0d431251449374c5d3fc709301662266f889a77c.

--- lib/cube_demo_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'cube_demo_state.dart';
-
-class CubeDemoCubit extends Cubit<CubeDemoState> {
-  CubeDemoCubit() : super(CubeDemoInitial());
-}
--- lib/cube_demo_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'cube_demo_cubit.dart';
-
-@immutable
-abstract class CubeDemoState {}
-
-class CubeDemoInitial extends CubeDemoState {}
