bab5527fa68d7257b3f0164b7e7e09a9ab994415
erikg <gutierrezerik971@gmail.com>
2023-10-23 19:55:08 -0500

    sssjnjnnnnmm

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Riverpod Counter'),
-        ),
         body: Center(
           child: Text('Hello World'),
         ),
