0f21376330cf2b482468d1c71ad9f5c5aff24b69
AndroidDevErik <ergutier@syr.edu>
2023-11-09 19:08:49 -0600

    added dependency injection

--- lib/presentation/onboarding/onboarding/view/onboarding_screen.dart
+++ lib/presentation/onboarding/onboarding/view/onboarding_screen.dart
@@ -1,10 +1,8 @@
-import 'dart:async';
-
 import 'package:bloc_demo/presentation/onboarding/onboarding/bloc/state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
+import 'package:local_auth/local_auth.dart';
 
 import '../../../../di/dependency_registry.dart';
 import '../../../resources/color_manager.dart';
@@ -23,13 +21,16 @@ class OnboardingScreen extends StatefulWidget {
   State<OnboardingScreen> createState() => _OnboardingScreenState();
 }
 
-class _OnboardingScreenState extends State<OnboardingScreen> {
+class _OnboardingScreenState extends State<OnboardingScreen> with WidgetsBindingObserver {
   late final OnBoardingBloc _bloc;
   final PageController _pageController = PageController(initialPage: 0);
+  final LocalAuthentication auth = LocalAuthentication();
 
   @override
-  void initState() {
+  initState() {
     super.initState();
+    _authenticate();
+    WidgetsBinding.instance.addObserver(this);
     _bloc = instance.get<OnBoardingBloc>()
       ..add(OnBoardingEventDefault())
       ..events.listen((event) { _handleEffect(event); });
@@ -71,7 +72,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     );
   }
 
-  void _handleEffect(OnBoardingEffect effect) {
+  _handleEffect(OnBoardingEffect effect) {
     switch(effect) {
       case OnBoardingEffectNext(): _pageController.nextPage(
           duration: DurationConstants.pageTransitionDuration,
@@ -83,5 +84,49 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
         );
       case OnBoardingEffectSkip() || OnBoardingEffectFinish(): Navigator.pushNamed(context, Routes.loginRoute);
     }
+  }
+
+  @override
+  didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    switch (state) {
+      case AppLifecycleState.resumed:
+      // Do something when the app is resumed
+        break;
+      case AppLifecycleState.inactive:
+      // The app is inactive
+        break;
+      case AppLifecycleState.paused:
+      // The app is paused
+        break;
+      case AppLifecycleState.detached:
+      // The app is detached
+        break;
+      case AppLifecycleState.hidden:
+        // The app is hidden
+    }
+  }
+
+  void _authenticate() async {
+    final bool canAuthenticateWithBiometrics = await auth.canCheckBiometrics;
+    final bool canAuthenticate = canAuthenticateWithBiometrics || await auth.isDeviceSupported();
+
+    if (canAuthenticate) {
+      bool didAuthenticate = await auth.authenticate(
+        localizedReason: 'Please authenticate to log in',
+        options: const AuthenticationOptions(biometricOnly: true),
+      );
+
+      if (didAuthenticate) {
+        // Proceed to the app
+      }
+    }
+  }
+
+  @override
+  dispose() {
+    _pageController.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
   }
 }
